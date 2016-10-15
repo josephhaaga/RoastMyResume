7f82f6621341b66bb70abfa91f5bf8e32d61e21c
Joseph Haaga <haaga.joe@gmail.com>
2016-10-15 17:54:31 -0400

    fix textarea woes

--- resume.js
+++ resume.js
@@ -11,23 +11,16 @@ function registerComment(x,y){
 	// Write to Firebase if field has text entered & category set
 	// use imgur image ID as /root/
 	// console.log("registerComment("+x+","+y+") began");
-	var text_field_val = $('#comment-field').val()
-	if(text_field_val.replace("\n","").length>0 && $(':checked').length>0){
+	var text_field_val = $('#comment-field').val();
+
+	var num_letters = text_field_val.match(/[\w]/g).length;
+
+	if(num_letters>0 && $(':checked').length>0){
 		$('#comment-submit').addClass("success").text("Success!");
 		annotation_status[annotation_number]=true;
 		var users_remarks = $('#comment-field').val();
-		// var users_remarks = $('#comment-field').val().replace(/<script[^>]*?>.*?<\/script>/gi, '').
-		// 			 replace(/<[\/\!]*?[^<>]*?>/gi, '').
-		// 			 replace(/<style[^>]*?>.*?<\/style>/gi, '').
-		// 			 replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '');
 		console.log("text: "+users_remarks);
 		annotations[annotation_number] = {'type':$(':checked').first().attr('id'), 'remarks':users_remarks};
-		
-		// add to DOM again since annotation dot disappeared
-		if($('#annotation'+annotation_number).length==0){
-			$('.resume-container').append("<div class='red-dot-light' id='annotation"+annotation_number+"' style='position:absolute;left:"+(x-10)+"px;top:"+(y-10)+"px'></div>");
-		}
-
 		setTimeout(
 			function(){
 				$('#comment-field').val('');
@@ -40,14 +33,18 @@ function registerComment(x,y){
 		return true;
 	}else{
 		console.log("no text in field");
-		console.log($(':checked').first().attr('id'));
-		switch($(':checked').first().attr('id')){
-			case 'grammar':
-				$('.resume-container').append("<div class='blue-dot-light' id='annotation"+annotation_number+"' style='position:absolute;left:"+(x-10)+"px;top:"+(y-10)+"px'></div>");
-			case 'design':
-				$('.resume-container').append("<div class='green-dot-light' id='annotation"+annotation_number+"' style='position:absolute;left:"+(x-10)+"px;top:"+(y-10)+"px'></div>");
-			case 'content':
-				$('.resume-container').append("<div class='red-dot-light' id='annotation"+annotation_number+"' style='position:absolute;left:"+(x-10)+"px;top:"+(y-10)+"px'></div>");
+		if($('#annotation'+annotation_number).length==0){
+			// $('.resume-container').append("<div class='red-dot-light' id='annotation"+annotation_number+"' style='position:absolute;left:"+(x-10)+"px;top:"+(y-10)+"px'></div>");
+			selected_type=$(':checked').first().attr('id'); 
+			console.log(selected_type);
+			switch(selected_type){
+				case "grammar":
+					$('.resume-container').append("<div class='blue-dot-light' id='annotation"+annotation_number+"' style='position:absolute;left:"+(x-10)+"px;top:"+(y-10)+"px'></div>");
+				case "design":
+					$('.resume-container').append("<div class='green-dot-light' id='annotation"+annotation_number+"' style='position:absolute;left:"+(x-10)+"px;top:"+(y-10)+"px'></div>");
+				case "content":
+					$('.resume-container').append("<div class='red-dot-light' id='annotation"+annotation_number+"' style='position:absolute;left:"+(x-10)+"px;top:"+(y-10)+"px'></div>");
+			}
 		}
 
 		$('#comment-submit').addClass("warning").text("Forget something?");
@@ -63,24 +60,25 @@ function createNewComment(x,y,anno){
 	$('#comment-field').focus();
 	$(document).keypress(function(e) {
 	    if(e.which == 13) {
-	    	if(!registerComment(x,y)){
-	    		$('#annotation'+annotation_number).detach();
-	    	}
+	    	// if(!registerComment(x,y)){
+	    	// 	$('#annotation'+annotation_number).detach();
+	    	// }
+	    	registerComment(x,y);
 	    }
 	});
 	$('#comment-submit').click(function(){
-		if(!registerComment(x,y)){
-	    	$('#annotation'+annotation_number).detach();
-    	}
+		// if(!registerComment(x,y)){
+	 //    	$('#annotation'+annotation_number).detach();
+  //   	}
+  		registerComment(x,y);
 	});
 }
 
 $('.the-resume').click(function(e){
 	var x = e.pageX;
 	var y = e.pageY;
-	$('.resume-container').append("<div class='red-dot-light' id='annotation"+annotation_number+"' style='position:absolute;left:"+(x-10)+"px;top:"+(y-10)+"px'></div>");
+	$('.resume-container').append("<div class='black-dot-light' id='annotation"+annotation_number+"' style='position:absolute;left:"+(x-10)+"px;top:"+(y-10)+"px'></div>");
 	createNewComment(x,y,annotation_number);
-	// console.log(createNewComment(x,y,annotation_number));
 });
 
 $('#comment-modal').on("closed.zf.reveal",function(){
