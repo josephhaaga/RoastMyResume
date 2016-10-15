03e0c086f1bd97eab21a3b8855e99f4480a1155b
Joseph Haaga <haaga.joe@gmail.com>
2016-10-15 13:01:28 -0400

    mvp started

---
+++ resume.js
@@ -0,0 +1,42 @@
+function registerComment(x,y){
+	// Write to Firebase if field has text entered & category set
+	// use imgur image ID as /root/
+	var text_field_val = $('#comment-field').val()
+	if(text_field_val.length>0){
+		$('#comment-submit').addClass("success").text("Success!");
+		setTimeout(
+			function(){
+				$('#comment-field').val('');
+				$('#comment-modal').foundation('close');
+				$('#comment-submit').removeClass("success").text('Submit');
+			}
+		,800);
+		console.log("Comment entered!");
+
+	}else{
+		console.log("no text in field");
+	}
+}
+
+function createNewComment(x,y){
+	console.log("comment added at "+x+", "+y);
+	$('#comment-modal').foundation('open');
+	$('#comment-field').focus();
+
+	$(document).keypress(function(e) {
+	    if(e.which == 13) {
+	    	registerComment(x,y);
+	    }
+	});
+
+	$('#comment-submit').click(function(){
+		registerComment(x,y);
+	})
+}
+
+$('.the-resume').click(function(e){
+	// window.alert("clicked at "+e.pageX+" "+e.pageY);
+	var x = e.pageX;
+	var y = e.pageY;
+	createNewComment(x,y);
+});
