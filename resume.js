8445b8c0eb56f6a00001fb8bec86157745299eb9
josephhaaga <haaga.joe@gmail.com>
2017-05-16 21:02:23 -0400

    Firebase is working!!!

--- resume.js
+++ resume.js
@@ -1,28 +1,29 @@
-var annotation_number=1;
 annotation_status = [];
 annotations = {};
+var this_resume_id = 123;
+
 
 function addToSidebar(anno_obj){
-	console.log(anno_obj['type']);
-	switch(anno_obj['type']){
-		case "grammar":
-			$('.sidebar').append("<div class='callout sidebar-annotation' id='sidebarAnnotation"+annotation_number+"' style='background-color:rgba(0,0,240,0.5);'>"+anno_obj.remarks+"</div>");
-			break;
-		case "design":
-			$('.sidebar').append("<div class='callout sidebar-annotation' id='sidebarAnnotation"+annotation_number+"' style='background-color:rgba(0,240,0,0.5);'>"+anno_obj.remarks+"</div>");
-			break;
-		case "content":
-			// $('.sidebar').append("<div class='callout alert'>"+anno_obj.remarks+"</div>");
-			$('.sidebar').append("<div class='callout sidebar-annotation' id='sidebarAnnotation"+annotation_number+"' style='background-color:rgba(240,0,0,0.5);'>"+anno_obj.remarks+"</div>");
-			break;
-	}	
+	console.log('addToSidebar()');
+	// switch(anno_obj['type']){
+	// 	case "grammar":
+	// 		$('.sidebar').append("<li><div class='callout sidebar-annotation' id='sidebarAnnotation"+annotation_number+"' style='background-color:rgba(0,0,240,0.5);'>"+anno_obj.remarks+"</div></li>");
+	// 		break;
+	// 	case "design":
+	// 		$('.sidebar').append("<div class='callout sidebar-annotation' id='sidebarAnnotation"+annotation_number+"' style='background-color:rgba(0,240,0,0.5);'>"+anno_obj.remarks+"</div>");
+	// 		break;
+	// 	case "content":
+	// 		// $('.sidebar').append("<div class='callout alert'>"+anno_obj.remarks+"</div>");
+	// 		$('.sidebar').append("<div class='callout sidebar-annotation' id='sidebarAnnotation"+annotation_number+"' style='background-color:rgba(240,0,0,0.5);'>"+anno_obj.remarks+"</div>");
+	// 		break;
+	// }	
 }
 
 
 function registerComment(x,y){
+	console.log("registerComment("+x+","+y+")");
 	// Write to Firebase if field has text entered & category set
 	// use imgur image ID as /root/
-	// console.log("registerComment("+x+","+y+") began");
 	var text_field_val = $('#comment-field').val();
 
 	var num_letters = text_field_val.match(/[\w]/g).length;
@@ -31,7 +32,6 @@ function registerComment(x,y){
 		$('#comment-submit').addClass("success").text("Success!");
 		annotation_status[annotation_number]=true;
 		var users_remarks = $('#comment-field').val();
-		console.log("text: "+users_remarks);
 		annotations[annotation_number] = {'type':$(':checked').first().attr('id'), 'remarks':users_remarks};
 		setTimeout(
 			function(){
@@ -41,15 +41,12 @@ function registerComment(x,y){
 				$('#comment-submit').removeClass("success").text('Submit');
 			}
 		,600);
-		console.log("Comment entered!");
 		addToSidebar(annotations[annotation_number]);
+		firebase.database().ref('resume/'+this_resume_id+'/posts/'+annotation_number).update(annotations[annotation_number]);
 		return true;
 	}else{
-		console.log("no text in field");
 		if($('#annotation'+annotation_number).length==0){
-			// $('.resume-container').append("<div class='red-dot-light' id='annotation"+annotation_number+"' style='position:absolute;left:"+(x-10)+"px;top:"+(y-10)+"px'></div>");
 			selected_type=$(':checked').first().attr('id'); 
-			console.log(selected_type);
 			switch(selected_type){
 				case "grammar":
 					$('.resume-container').append("<div class='blue-dot-light' id='annotation"+annotation_number+"' style='position:absolute;left:"+(x-10)+"px;top:"+(y-10)+"px'></div>");
@@ -72,11 +69,14 @@ function registerComment(x,y){
 }
 
 function createNewComment(x,y,anno){
+	console.log("createNewComment("+x+","+y+","+anno+")");
 	$('#comment-modal').foundation('open');
 	$('#comment-field').focus();
 }
 
 $('.the-resume').click(function(e){
+	annotation_number = firebase.database().ref('resume/'+this_resume_id).child('posts').push().key;
+	console.log("current_anno: "+annotation_number);
 	x = e.pageX;
 	y = e.pageY;
 	$('.resume-container').append("<div class='black-dot-light' id='annotation"+annotation_number+"' style='position:absolute;left:"+(x-10)+"px;top:"+(y-10)+"px'></div>");
@@ -84,24 +84,26 @@ $('.the-resume').click(function(e){
 });
 
 $('#comment-modal').on("closed.zf.reveal",function(){
-	if(!annotation_status[annotation_number]==true){
-		$('#annotation'+annotation_number).detach();
-		$(':checked').first().prop('checked',false);
-		$('#comment-field').val("");
+	// if(!annotation_status[annotation_number]==true){
+	// 	$('#annotation'+annotation_number).detach();
+	// 	$(':checked').first().prop('checked',false);
+	// 	$('#comment-field').val("");
+	// }
+});
+
+// Style Annotation dot to match annotation_type
+$(".commentType li").click(function(){
+	switch($(this).prop('id')){
+		case 'commentContent':
+			$('#annotation'+annotation_number).removeClass().addClass("red-dot-light");
+			break;
+		case 'commentGrammar':
+			$('#annotation'+annotation_number).removeClass().addClass("blue-dot-light");		
+			break;
+		default:
+			$('#annotation'+annotation_number).removeClass().addClass("green-dot-light");		
+			break;
 	}
-	annotation_number+=1;
-});
-
-$('#comment-modal #commentGrammar').click(function(){
-	$('#annotation'+annotation_number).removeClass().addClass("blue-dot-light");
-});
-
-$('#comment-modal #commentContent').click(function(){
-	$('#annotation'+annotation_number).removeClass().addClass("red-dot-light");
-});
-
-$('#comment-modal #commentDesign').click(function(){
-	$('#annotation'+annotation_number).removeClass().addClass("green-dot-light");
 });
 
 $(document).keypress(function(e) {
