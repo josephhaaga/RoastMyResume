91e6b5325a4b3cf877a73e90d4223703b796cc14
josephhaaga <haaga.joe@gmail.com>
2017-05-29 10:50:56 -0400

    master resume list

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 $(document).foundation()
+
+firebase.database().ref('resume').on("value", function(snapshot) {
+  resumes = (snapshot.val());
+},function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
+
