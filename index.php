043f16a781c41e0474bbe9ceb145e28f205f3474
josephhaaga <haaga.joe@gmail.com>
2017-05-29 15:21:50 -0400

    index link to appjs

--- index.php
+++ index.php
@@ -75,6 +75,7 @@
     <script src="js/vendor/jquery.js"></script>
     <script src="js/vendor/what-input.js"></script>
     <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
 
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
