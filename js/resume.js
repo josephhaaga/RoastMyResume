27dcf0d680b265b1139409ebb9d4e84bcdaa9673
josephhaaga <haaga.joe@gmail.com>
2017-05-28 22:53:35 -0400

    update resume img according to url parameters

--- js/resume.js
+++ js/resume.js
@@ -5,7 +5,7 @@ var url = window.location.href;
 var captured = /resumeId=([^&]+)/.exec(url)[1]; // Value is in [1] ('384' in our case)
 var this_resume_id = captured ? captured : '123';
 
-$('.the-resume').attr("src", "uploads/"+this_resume_id);
+$('.the-resume').attr("src", "uploads/"+this_resume_id+".jpg");
 
 var starCountRef = firebase.database().ref('resume/' + this_resume_id + '/posts');
 starCountRef.on('value', function(snapshot) {
